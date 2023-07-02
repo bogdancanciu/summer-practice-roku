ab315e4c36c1c160e0250a918baebc7b06bb16b6
bogdancanciu <bogdancanciu21@gmail.com>
2023-07-02 22:23:21 +0300

    Remove node dependencies

--- node_modules/brighterscript/dist/bscPlugin/completions/CompletionsProcessor.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ProvideCompletionsEvent } from '../../interfaces';
-export declare class CompletionsProcessor {
-    private event;
-    constructor(event: ProvideCompletionsEvent);
-    process(): void;
-}
